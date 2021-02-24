fd0f7855056cfb87b1392d9cd9bcf435723ec274
julio cesar daal de sousa <juliocesar.daal@gmail.com>
2021-02-24 23:51:50 +0100

    Testing Heroku Config

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ app.use(session({
     saveUninitialized: true
 }));
 
+//Config parameters
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
